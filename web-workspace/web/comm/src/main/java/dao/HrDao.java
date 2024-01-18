d88c87d2d44194fdc829e258fe6e083b020c666f
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-01-18 17:03:42 +0900

    20240118 Day54 JSON - 인사관리 게시판, 박스오피스 게시판, 외부API

---
+++ web-workspace/web/comm/src/main/java/dao/HrDao.java
@@ -0,0 +1,24 @@
+package dao; //20240118
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+import ibatis.IbatisUtil;
+import vo.Department;
+import vo.Employee;
+
+public class HrDao {
+	private SqlMapClient ibatis = IbatisUtil.getSqlMapClient();
+	
+	@SuppressWarnings("unchecked")
+	public List<Department> getAllDepartments() throws SQLException {
+		return (List<Department>) ibatis.queryForList("hr.getAllDepartments");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Employee> getEmployeesByDepartmentId(int deptId) throws SQLException {
+		return (List<Employee>) ibatis.queryForList("hr.getEmployeesByDepartmentId", deptId);
+	}
+}
